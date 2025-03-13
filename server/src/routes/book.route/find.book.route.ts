39e0fc6870ebfa40c8543ef87cca17ea326c340e
Yash Gupta <mind0bender@gmail.com>
2025-03-13 18:47:51 +0530

    find books with filters

---
+++ server/src/routes/book.route/find.book.route.ts
@@ -0,0 +1,62 @@
+import { Request, Response } from "express";
+import Book, { BookInterface } from "../../db/models/book.model";
+import { z } from "zod";
+import { ResponseType } from "../../utils/response.util";
+import errorsFromZodIssue from "../../utils/validation.util/error.validation.util";
+import { Document } from "mongoose";
+
+const findBookSchema = z.object({
+  filter: z
+    .object({
+      title: z.string().optional(),
+    })
+    .optional(),
+});
+
+export default function findBookRoute(
+  req: Request,
+  res: Response<
+    ResponseType<{
+      books: Omit<BookInterface, keyof Document>[];
+    }>
+  >
+): void {
+  const { filter } = req.body;
+  const { success, data, error } = findBookSchema.safeParse({ filter });
+  if (!success) {
+    res.json({ success: false, errors: errorsFromZodIssue(error) });
+    return;
+  }
+  Book.find(
+    data.filter
+      ? {
+          title: data.filter.title,
+        }
+      : {}
+  )
+    .then((books: BookInterface[]): void => {
+      res.json({
+        success: true,
+        data: {
+          books: books.map(
+            (book: BookInterface): Omit<BookInterface, keyof Document> => ({
+              title: book.title,
+              authors: book.authors,
+              genres: book.genres,
+              rating: book.rating,
+              summary: book.summary,
+              coverImageUrl: book.coverImageUrl,
+              copies: book.copies,
+              isbn: book.isbn,
+              pages: book.pages,
+            })
+          ),
+        },
+      });
+      return;
+    })
+    .catch((error: Error): void => {
+      res.json({ success: false, errors: [error.message] });
+      return;
+    });
+}
--- server/src/routes/book.route/index.book.route.ts
+++ server/src/routes/book.route/index.book.route.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import addBookRoute from "./add.book.route";
+import findBookRoute from "./find.book.route";
 
 const bookRouter: Router = Router();
 
 bookRouter.post("/add", addBookRoute);
+bookRouter.post("/find", findBookRoute);
 
 export default bookRouter;
